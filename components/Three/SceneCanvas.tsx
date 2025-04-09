9cbe3aea951c34c282fe9ad64606a5935ac39c09
mohiitprasher123 <mohitprshr0@gmail.com>
2025-04-09 02:55:02 +0000

    Corrected imports and refined SceneCanvas

--- components/Three/SceneCanvas.tsx
+++ components/Three/SceneCanvas.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
+import { OrbitControls, Environment, Float } from '@react-three/drei';
 import ParticlesBackground from './ParticlesBackground';
 import { FuturisticSphere } from '../FuturisticSphere';
 import { DNAHelix } from '../DNAHelix';
@@ -13,8 +13,12 @@ export default function SceneCanvas() {
       <ambientLight intensity={0.5} />
       <directionalLight intensity={1.2} position={[5, 10, 5]} />
       <group position={[0, -0.5, 0]}>
-        <FuturisticSphere />
-        <DNAHelix />
+        <Float speed={2} rotationIntensity={0.8} floatIntensity={1}>
+          <FuturisticSphere />
+        </Float>
+        <Float speed={1.5} rotationIntensity={0.6}>
+          <DNAHelix />
+        </Float>
       </group>
       <ParticlesBackground />
     </Canvas>
