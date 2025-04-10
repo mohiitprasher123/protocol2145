cb09c98c14c1dc9fe1157dc35e8d671885aa1449
mohiitprasher123 <mohitprshr0@gmail.com>
2025-04-10 18:29:01 +0000

    Final fix for 3D rendering

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    turbo: {
-      loaders: {},
-      rules: {},
-    },
-  },
-};
+    appDir: true
+  }
+}
 
 module.exports = nextConfig;
